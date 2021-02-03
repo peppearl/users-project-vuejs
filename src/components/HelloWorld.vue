d2f52423363e0068398ef72e1b03f37a48176b4e
Gabrielle Geroudet <gabrielle.geroudet@gmail.com>
2021-02-03 14:54:03 +0100

    Query Params working

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,10 +9,17 @@
     >
       Afficher 20 nouveaux utilisateurs
     </button>
+    <button
+        class="btn btn-outline-dark"
+        @click="resetFilter"
+    >
+      reset
+    </button>
     <br>
     <label>
       <input
           v-model="genderFilter"
+          :disabled="genderFilter.length<2 && genderFilter.includes('male')"
           type="checkbox"
           value="male"
       >
@@ -21,6 +28,7 @@
     <label>
       <input
           v-model="genderFilter"
+          :disabled="genderFilter.length<2 && genderFilter.includes('female')"
           type="checkbox"
           value="female"
       >
@@ -42,12 +50,18 @@
       <label>Trier par âge :
       </label>
       <p v-if="currentSortDir === ''">Par défaut</p>
-      <p v-if="currentSortDir === 'asc'">Croissant</p>
-      <p v-if="currentSortDir === 'desc'">Décroissant</p>
+      <p v-else-if="currentSortDir === 'asc'">Croissant</p>
+      <p v-else>Décroissant</p>
     </div>
-    <p v-if="results.length">il y a <strong>{{ getFilteredNames.length }}</strong> utilisateurs</p>
-    <p v-else>il n'y a <strong>aucun</strong> utilisateur</p>
+    <p v-if="results.length">
+      il y a <strong>{{ sortedAge.length }}</strong> utilisateur{{ sortedAge.length > 1 ? 's' : '' }}
+      filtré{{ sortedAge.length > 1 ? 's' : '' }} sur {{ results.length }} totaux
+    </p>
+    <p v-else>
+      il n'y a <strong>aucun</strong> utilisateur récupéré
+    </p>
     <table
+        v-if="results.length"
         id="tbl-users"
         class="table table-hover"
     >
@@ -58,44 +72,44 @@
         <th>Email</th>
         <th>Tel</th>
         <th>Genre</th>
-        <th @click="sort('age')">
+        <th @click="sortBy('age')">
           <span v-if="currentSortDir === 'desc'">
             <svg
                 data-v-1061b08a=""
-                width="16"
                 height="16"
-                xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 320 512"
+                width="16"
+                xmlns="http://www.w3.org/2000/svg"
             ><path
+                d="M279 224H41c-21.4 0-32.1-25.9-17-41L143 64c9.4-9.4 24.6-9.4 33.9 0l119 119c15.2 15.1 4.5 41-16.9 41z"
                 data-v-1061b08a=""
                 fill="currentColor"
-                d="M279 224H41c-21.4 0-32.1-25.9-17-41L143 64c9.4-9.4 24.6-9.4 33.9 0l119 119c15.2 15.1 4.5 41-16.9 41z"
             /></svg>
           </span>
           <span v-else-if="currentSortDir === 'asc'">
             <svg
                 data-v-1061b08a=""
-                width="16"
                 height="16"
-                xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 320 512"
+                width="16"
+                xmlns="http://www.w3.org/2000/svg"
             ><path
+                d="M41 288h238c21.4 0 32.1 25.9 17 41L177 448c-9.4 9.4-24.6 9.4-33.9 0L24 329c-15.1-15.1-4.4-41 17-41z"
                 data-v-1061b08a=""
                 fill="currentColor"
-                d="M41 288h238c21.4 0 32.1 25.9 17 41L177 448c-9.4 9.4-24.6 9.4-33.9 0L24 329c-15.1-15.1-4.4-41 17-41z"
             /></svg>
           </span>
           <span v-else>
             <svg
                 data-v-1061b08a=""
-                width="16"
                 height="16"
-                xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 320 512"
+                width="16"
+                xmlns="http://www.w3.org/2000/svg"
             ><path
+                d="M41 288h238c21.4 0 32.1 25.9 17 41L177 448c-9.4 9.4-24.6 9.4-33.9 0L24 329c-15.1-15.1-4.4-41 17-41zm255-105L177 64c-9.4-9.4-24.6-9.4-33.9 0L24 183c-15.1 15.1-4.4 41 17 41h238c21.4 0 32.1-25.9 17-41z"
                 data-v-1061b08a=""
                 fill="currentColor"
-                d="M41 288h238c21.4 0 32.1 25.9 17 41L177 448c-9.4 9.4-24.6 9.4-33.9 0L24 329c-15.1-15.1-4.4-41 17-41zm255-105L177 64c-9.4-9.4-24.6-9.4-33.9 0L24 183c-15.1 15.1-4.4 41 17 41h238c21.4 0 32.1-25.9 17-41z"
             ></path>
             </svg>
           </span>
@@ -131,10 +145,10 @@ export default {
     return {
       results: [],
       errored: false,
-      genderFilter: ['male', 'female'],
-      filterByName: '',
+      genderFilter: (this.$route.query.gender || 'male,female').split(','),
+      filterByName: this.$route.query.search || '',
       currentSort: 'age',
-      currentSortDir: '',
+      currentSortDir: this.$route.query.sort || '',
     }
   },
   computed: {
@@ -149,7 +163,7 @@ export default {
     sortedAge() {
       return this.getFilteredNames.slice().sort((a, b) => {
         if (!this.currentSortDir) return 0;
-        const  modifier = this.currentSortDir === 'desc' ? -1 : 1;
+        const modifier = this.currentSortDir === 'desc' ? -1 : 1;
         return (a.dob.age - b.dob.age) * modifier;
       });
     },
@@ -170,8 +184,8 @@ export default {
             this.errored = true
           })
     },
-    sort(s) {
-      //if s == current sort, reverse
+    sortBy(s) {
+      //if s == current sortBy, reverse
       if (s === this.currentSort) {
         if (this.currentSortDir === '') {
           this.currentSortDir = 'asc'
@@ -184,6 +198,35 @@ export default {
         this.currentSort = s;
       }
     },
+    resetFilter() {
+      this.genderFilter = ['male', 'female'];
+      this.filterByName = '';
+      this.currentSortDir = ''
+    },
+    updateQuery() {
+      const query = {};
+      if (this.genderFilter.length < 2) {
+        query.gender = this.genderFilter.join(',')
+      }
+      if (this.filterByName) {
+        query.search = this.filterByName
+      }
+      if (this.currentSortDir) {
+        query.sort = this.currentSortDir
+      }
+      this.$router.push({query})
+    }
+  },
+  watch: {
+    genderFilter() {
+      this.updateQuery()
+    },
+    filterByName() {
+      this.updateQuery()
+    },
+    currentSortDir() {
+      this.updateQuery()
+    }
   },
 }
 </script>
