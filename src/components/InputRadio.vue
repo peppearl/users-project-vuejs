9285654812135060dead7d897bf3b98be2602c22
Gabrielle Geroudet <gabrielle.geroudet@gmail.com>
2021-03-14 15:14:47 +0100

    Creating Input components + PUT

---
+++ src/components/InputRadio.vue
@@ -0,0 +1,17 @@
+<template>
+  <label>
+    {{label}}
+    <input type="radio" :value="value" :name="name">
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'InputRadio',
+  props: [
+    "label",
+    "value",
+    "name"
+  ]
+}
+</script>
